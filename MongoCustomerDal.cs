87e69a65f0e27a54ce5947ce6f582f25b7987f85
Berkant Gunes <gunesberkant96@gmail.com>
2025-05-04 10:39:21 +0300

    Add files via upload

---
+++ MongoCustomerDal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class MongoCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            string durum = "Kaydedildi";
+
+        }
+
+        public int Count()
+        {
+            return 10;
+        }
+
+        public void Delete()
+        {
+            string durum = "Silindi";
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+          List<Customer> customerList = new List<Customer>();
+            foreach (var item in customerList)
+            {
+                customerList.Add(item);
+            }
+            return customerList;
+        }
+
+        public void Update()
+        {
+            string durum = "Güncellendi";
+        }
+    }
+}
---
+++ OracleCustomerDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            string durum = "Kaydedildi";
+        }
+
+        public int Count()
+        {
+            return 10;
+        }
+
+        public void Delete()
+        {
+            string durum = "Silindi";
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+           List<Customer> customers = new List<Customer>();
+            return customers;
+        }
+
+        public void Update()
+        {
+            string durum = "Güncellendi";
+        }
+    }
+}
