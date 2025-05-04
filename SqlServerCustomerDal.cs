65568d5d06fb515f76b08e744e219ad0768ec9e0
Berkant Gunes <gunesberkant96@gmail.com>
2025-05-04 10:39:34 +0300

    Add files via upload

---
+++ SqlServerCustomerDal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            string durum = "Kaydedildi";
+        }
+
+        public int Count()
+        {
+            return 10;
+        }
+
+        public void Delete()
+        {
+            string durum = "Silindi";
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            List<Customer> customerList = new List<Customer>();
+            return customerList;
+
+        }
+
+        public void Update()
+        {
+            string durum = "Güncellendi";
+        }
+    }
+}
